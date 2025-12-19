a617083bdfb77363030823ac55972e3c4d2128c5
SoufainNajmi <soufainnajmi@gmail.com>
2025-12-19 23:52:58 +0100

    Supplier.php

--- models/Supplier.php
+++ models/Supplier.php
@@ -21,4 +21,33 @@ class Supplier {
         $stmt->execute();
         return $stmt;
     }
+
+     public function create() {
+        $query = "INSERT INTO " . $this->table . "
+                  SET name = :name,
+                      contact = :contact,
+                      email = :email,
+                      phone = :phone,
+                      address = :address,
+                      created_at = NOW()";
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->contact = htmlspecialchars(strip_tags($this->contact));
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->phone = htmlspecialchars(strip_tags($this->phone));
+        $this->address = htmlspecialchars(strip_tags($this->address));
+
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':contact', $this->contact);
+        $stmt->bindParam(':email', $this->email);
+        $stmt->bindParam(':phone', $this->phone);
+        $stmt->bindParam(':address', $this->address);
+
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
 }
