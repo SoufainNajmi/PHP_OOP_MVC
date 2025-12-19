eac5fdada8a39816d4a67ce2a1bd85cfe9749e0a
SoufainNajmi <soufainnajmi@gmail.com>
2025-12-19 23:18:25 +0100

    read function

--- models/Supplier.php
+++ models/Supplier.php
@@ -14,4 +14,11 @@ class Supplier {
     public function __construct($db) {
         $this->conn = $db;
     }
+
+     public function read() {
+        $query = "SELECT * FROM " . $this->table . " ORDER BY created_at DESC";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
 }
