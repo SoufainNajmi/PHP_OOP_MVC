167ee01d0f8bd9c2c2aa7f99d014b0237fe467f4
SoufainNajmi <soufainnajmi@gmail.com>
2025-12-22 23:11:57 +0100

    read

--- models/StockMovement.php
+++ models/StockMovement.php
@@ -14,5 +14,16 @@ class StockMovement {
     public function __construct($db) {
         $this->conn = $db;
     }
+
+      public function read() {
+        $query = "SELECT sm.*, p.name as product_name 
+                  FROM " . $this->table . " sm
+                  LEFT JOIN products p ON sm.product_id = p.id
+                  ORDER BY sm.created_at DESC";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
 }
 ?>
