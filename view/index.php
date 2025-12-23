2ee741af1e200c465c90672d00df1cfc4ceed9f3
SoufainNajmi <soufainnajmi@gmail.com>
2025-12-23 22:00:49 +0100

    view

---
+++ view/index.php
@@ -0,0 +1,5 @@
+<?php ob_start(); ?>
+<div class="d-flex justify-content-between align-items-center mb-4">
+    <h1>Gestion des Produits</h1>
+    <a href="index.php?action=create_product" class="btn btn-primary">Ajouter un Produit</a>
+</div>
