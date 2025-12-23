e19d6fb09db2bdf905912ab9ee91b71c37db7a81
SoufainNajmi <soufainnajmi@gmail.com>
2025-12-23 22:08:10 +0100

    dash

---
+++ view/dashboard.php
@@ -0,0 +1,7 @@
+<?php ob_start(); ?>
+<div class="row mb-4">
+    <div class="col-md-12">
+        <h1>Tableau de Bord</h1>
+        <p class="lead">Vue d'ensemble de votre gestion de stock</p>
+    </div>
+</div>
